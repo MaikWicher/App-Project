8697bd9c79031a58cc0a554e7dcd28f0a9816192
MaikWicher <gdx165462@student.gdansk.merito.pl>
2026-01-16 17:58:17 +0100

    Dodawanie Electron

---
+++ Electron/main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+let mainWindow: BrowserWindow | null = null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    backgroundColor: "#1e1e1e",
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true
+    }
+  });
+
+  // DEV
+  if (process.env.VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL);
+    mainWindow.webContents.openDevTools();
+  } 
+  // PROD
+  else {
+    mainWindow.loadFile("../Frontend/dist/index.html");
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
